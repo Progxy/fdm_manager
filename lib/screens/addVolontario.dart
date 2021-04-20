2d6f811019c8b02f84a350a7996a2bc49df961d0
Emanuele <70652793+Progxy@users.noreply.github.com>
2021-04-20 16:46:39 +0200

    addVolontari system implemented

---
+++ lib/screens/addVolontario.dart
@@ -0,0 +1,286 @@
+import 'dart:io';
+import 'package:connectivity/connectivity.dart';
+import 'package:fdm_manager/screens/home.dart';
+import 'package:fdm_manager/screens/mainDrawer.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'badConnection.dart';
+import 'feedback.dart';
+import 'utilizzo.dart';
+
+class AddVolontario extends StatefulWidget {
+  static const String routeName = "/addVolontario";
+
+  @override
+  _AddVolontarioState createState() => _AddVolontarioState();
+}
+
+class _AddVolontarioState extends State<AddVolontario> {
+  final List<String> choices = <String>[
+    "FeedBack",
+    "Aiuto",
+  ];
+
+  void choiceAction(String choice) async {
+    if (choice == "Aiuto") {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Utilizzo()));
+    } else {
+      var connectivityResult = await (Connectivity().checkConnectivity());
+      if (connectivityResult == ConnectivityResult.none) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => BadConnection()));
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => FeedBack()));
+      }
+    }
+  }
+
+  final TextStyle infoStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w300,
+    color: Colors.grey,
+  );
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  addDataToDatabase(String email, String name) {
+    final databaseReference =
+        FirebaseDatabase.instance.reference().child("Volontari");
+    databaseReference.set({name: email});
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = ((MediaQuery.of(context).size.width) / 3) * 2;
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Color.fromARGB(255, 192, 192, 192),
+        ),
+        title: Text(
+          "Aggiungi Volontari",
+          style: TextStyle(
+            color: Color.fromARGB(255, 192, 192, 192),
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: choiceAction,
+            itemBuilder: (BuildContext context) {
+              return choices.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(
+                    choice,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                );
+              }).toList();
+            },
+          )
+        ],
+        backgroundColor: Color.fromARGB(255, 24, 37, 102),
+        centerTitle: true,
+      ),
+      drawer: MainDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 15,
+            ),
+            Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: width * 1.35,
+                    child: TextFormField(
+                      controller: _nameController,
+                      maxLines: 20,
+                      decoration: const InputDecoration(
+                        hintText: "Inserire nome e cognome",
+                        hintStyle: TextStyle(
+                          fontSize: 23.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black87,
+                        ),
+                        border: OutlineInputBorder(),
+                        labelText: "Nome e Cognome",
+                        labelStyle: TextStyle(
+                          fontSize: 23.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Dati Mancanti";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: width * 1.35,
+                    child: TextFormField(
+                      controller: _emailController,
+                      maxLines: 20,
+                      decoration: const InputDecoration(
+                        hintText: "Inserire l'email",
+                        hintStyle: TextStyle(
+                          fontSize: 23.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black87,
+                        ),
+                        border: OutlineInputBorder(),
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          fontSize: 23.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Dati Mancanti";
+                        } else if (RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value)) {
+                          return "Email Invalida!";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: TextButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7.0),
+                  ),
+                  primary: Color.fromARGB(255, 24, 37, 102),
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    ProgressDialog dialog = new ProgressDialog(context);
+                    dialog.style(message: 'Caricamento...');
+                    await dialog.show();
+                    final String email = _emailController.text.trim();
+                    final String name = _nameController.text.trim();
+                    bool result = addDataToDatabase(email, name);
+                    await dialog.hide();
+                    if (Platform.isIOS) {
+                      showCupertinoDialog(
+                        context: context,
+                        builder: (BuildContext context) => CupertinoAlertDialog(
+                          title: Text(
+                            "Esito Operazione",
+                            style: TextStyle(
+                              fontSize: 28,
+                            ),
+                          ),
+                          content: Text(
+                            result
+                                ? "Operazione effetuata con successo !"
+                                : "Ops... Si è verificato un'errore mentre veniva spedita l'email.",
+                            style: TextStyle(
+                              fontSize: 28,
+                            ),
+                          ),
+                          actions: [
+                            CupertinoDialogAction(
+                              child: Text(
+                                "OK",
+                                style: TextStyle(
+                                  fontSize: 28,
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context, rootNavigator: true)
+                                    .pop('dialog');
+                              },
+                            )
+                          ],
+                        ),
+                      );
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: Text(
+                            "Esito Operazione",
+                            style: TextStyle(
+                              fontSize: 28,
+                            ),
+                          ),
+                          content: Text(
+                            result
+                                ? "Operazione effetuata con successo !"
+                                : "Ops... Si è verificato un'errore mentre veniva spedita l'email.",
+                            style: TextStyle(
+                              fontSize: 28,
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              child: Text(
+                                "OK",
+                                style: TextStyle(
+                                  fontSize: 28,
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context, rootNavigator: true)
+                                    .pop('dialog');
+                              },
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                    MaterialPageRoute(builder: (context) => Home());
+                  }
+                },
+                child: Text(
+                  "Accetta",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
